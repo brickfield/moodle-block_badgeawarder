c1a331ddb22f7ef6e187cc05bbade566a2f3c752
Mike Churchward <mike@brickfieldlabs.ie>
2023-04-10 16:28:49 -0400

    Base 4.1 version.

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$plugin->component = 'block_badgeawarder';
 $plugin->version   = 2019070900;
-$plugin->requires  = 2018051700;
+$plugin->requires  = 2022112800; // Moodle 4.1 and higher.
 $plugin->release = '3.5 (Build: 2018051700)';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->component = 'block_badgeawarder';
